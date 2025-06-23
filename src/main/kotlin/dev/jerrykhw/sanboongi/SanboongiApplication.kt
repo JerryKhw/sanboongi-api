e10d90217c9c79fc04ccdf0acc946410e3a95b43
JerryKhw <hwjameshw@gmail.com>
2025-06-23 14:54:30 +0900

    Init

---
+++ src/main/kotlin/dev/jerrykhw/sanboongi/SanboongiApplication.kt
@@ -0,0 +1,11 @@
+package dev.jerrykhw.sanboongi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SanboongiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SanboongiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/jerrykhw/sanboongi/SanboongiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.jerrykhw.sanboongi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SanboongiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
