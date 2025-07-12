b5b87d6bf5fe684f940a7be3a091a12aa7d09866
JerryKhw <hwjameshw@gmail.com>
2025-07-13 05:54:22 +0900

    fix: 초기 sql 수정

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -6,8 +6,7 @@ CREATE TABLE users (
     nickname VARCHAR(100) NOT NULL,
     social_id VARCHAR(255) NOT NULL,
     social_type VARCHAR(20) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE documents (
